3713165eef0c1177f8e55a50b324da204d999c5f
unknown <minhdico@gmail.com>
2023-04-28 21:07:48 -0400

    commit

--- hw_rest_api_post.py
+++ hw_rest_api_post.py
@@ -1,7 +1,7 @@
 import requests
 
 BASE = 'http://127.0.0.1:5000/'
-
-response = requests.post(BASE + 'example', {'pool': [1,7,2,6], 'percentile': 99.5})
+headers = {'accept': 'application/json'}
+response = requests.post(BASE + 'example', headers=headers, json={'pool': [1,7,2,6,8], 'percentile': 99.5})
 print(response.json())
 #[1,7,2,6]
